68cff28989252fe320c56b81ab7f38de6fdb4ecc
Andrei Nikiforov <79208362+anikod@users.noreply.github.com>
2023-04-04 18:33:37 +0300

    feat: gh-pages for vite project

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lorem_ipsum_vue/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
