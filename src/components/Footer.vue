56bcdd943dedd915e933a0dba5cadee67c6eb1bb
Andrei Nikiforov <79208362+anikod@users.noreply.github.com>
2023-04-07 05:19:23 +0300

    feat: template ver. 1.0

---
+++ src/components/Footer.vue
@@ -0,0 +1,117 @@
+<script>
+// import i1 from '../assets/icons/i-1.svg';
+</script>
+
+
+<template>
+  <div class="footer">
+
+    <p>&copy 2018 &laquo;LOREMIPSUM.NET&raquo; Все права <br> защищены.</p>
+
+    <img src="../assets/icons/vertical.svg" alt="">
+
+
+    <div class="footer__item footer__item_qiwi">QIWI wallet</div>
+    <div class="footer__item footer__item_ym">Yandex Money</div>
+    <div class="footer__item footer__item_wm">Web Money</div>
+
+    <img src="../assets/icons/vertical.svg" alt="">
+
+    <a href="mailto:info@ipsum228.com" class="footer__link"><div class="footer__item footer__item_mail">info@ipsum228.com</div></a>
+
+    <a href="https://vk.com" class="footer__link"><div class="footer__item footer__item_vk">Мы вконтакте</div></a>
+
+  </div>
+</template>
+
+
+<style lang="scss">
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  font-size: 0.8rem;
+  padding-top: 15px;
+  // padding: 0;
+  margin: 0;
+  // border: 1px solid red;
+
+  &__item {
+    font-weight: normal;
+    // margin: 0 auto;
+    text-align: center;
+    flex: 0 1 8%;
+    position: relative;
+    // border: 41x solid red;
+
+    &_qiwi::before {
+      content: "";
+      position: absolute;
+      left: -15px;
+      top: -3px;
+      background-image: url('../assets/icons/qiwi.svg');
+      background-repeat: no-repeat;
+      width: 100px;
+      height: 50px;
+    }
+
+    &_ym::before {
+      content: "";
+      position: absolute;
+      left: -20px;
+      top: -5px;
+      background-image: url('../assets/icons/ym.svg');
+      background-repeat: no-repeat;
+      width: 100px;
+      height: 50px;
+    }
+
+    &_wm::before {
+      content: "";
+      position: absolute;
+      left: -20px;
+      top: -5px;
+      background-image: url('../assets/icons/wm.svg');
+      background-repeat: no-repeat;
+      width: 100px;
+      height: 50px;
+    }
+
+    &_mail::before {
+      content: "";
+      position: absolute;
+      left: -30px;
+      top: 2px;
+      background-image: url('../assets/icons/mail.svg');
+      background-repeat: no-repeat;
+      width: 100px;
+      height: 50px;
+    }
+
+    &_vk::before {
+      content: "";
+      position: absolute;
+      left: -20px;
+      top: -0px;
+      background-image: url('../assets/icons/vk.svg');
+      background-repeat: no-repeat;
+      width: 100px;
+      height: 50px;
+    }
+
+    
+  }
+}
+
+a.footer__link, a.footer__link:link, a.footer__link:visited, a.footer__link a:active {
+  color: white;
+}
+
+@media (max-width: 320px) {
+  $bgcolor: none;
+
+  $color: teal;
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,132 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="header">
+
+    <div class="logo">
+      <img src="@/assets/logo.png" alt="LoremIpsum.Net">
+    </div>
+
+    <nav class="header-menu">
+      <ul class="header-menu__list">
+        <li class="header-menu__item"><a href="#" class="header-menu__link">Бизнес</a></li>
+        <li class="header-menu__item"><a href="#" class="header-menu__link">О нас</a></li>
+        <li class="header-menu__item"><a href="#" class="header-menu__link">Цены</a></li>
+        <li class="header-menu__item"><a href="#" class="header-menu__link">Оформить заказ</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+
+<style lang="scss">
+$textblue: #3E9CDC;
+
+// $color: teal;
+
+.header {
+  margin: 0;
+  padding: 0;
+  // background-color: rgba(16, 16, 29, 0.9);
+  // padding: 5px;
+  transition: all 0.5s;
+  height: 100px;
+  // max-width: 1920px;
+  // position: fixed;
+  // width: 1920px;
+  // max-width: 1920px;
+  // display: flex;
+  // width: 1920px;
+  // flex: 1 1 auto;
+  // display: flex;
+  // flex-direction: row;
+  // justify-content: space-between;
+  // border: 5px solid red;
+
+}
+
+
+.logo {
+  color: white;
+  display: inline;
+  // font-size: 2rem;
+  // height: 100px;
+  line-height: 100px;
+  margin-left: 5px;
+  // border: 1px solid red;
+  // vertical-align: middle;
+
+  // flex: 0 0 400px;
+
+  & img {
+    vertical-align: middle;
+  }
+
+}
+
+
+.header-menu {
+  width: 450px;
+  float: right;
+
+  &__list {
+    // border: 1px solid $color;
+    height: 100px;
+    // vertical-align: middle;
+    // border: 1px solid red;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    // width: 400px;
+    // height: 100px;
+
+  }
+
+  &__item,
+  &__item li {
+    // flex: 0 0 120px;
+    list-style: none;
+    // display: inline-block;
+    // flex-basis: 500px;
+    // flex: 1 1 auto;
+    // text-align: center;
+
+  }
+
+  &__link {
+    // border: 1px solid red;
+    // line-height: 100px;
+    padding: 1.2rem;
+    overflow: hidden;
+
+    color: white;
+    font-size: 0.75rem;
+    font-weight: normal;
+    text-transform: uppercase;
+    text-decoration: none;
+    transition: all 0.25s ease;
+  }
+
+  &__link:hover {
+    text-decoration: underline;
+    color: $textblue;
+  }
+
+  &__item:last-child a {
+    // font-size: 3rem;
+    padding-right: 0;
+    // margin: 0;
+    // border: 1px solid red;
+    letter-spacing: 0.07rem;
+    ;
+  }
+}
+
+@media (max-width: 320px) {}
+</style>
